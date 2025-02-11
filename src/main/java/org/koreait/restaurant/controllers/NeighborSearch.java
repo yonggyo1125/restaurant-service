4b3493d9f3df3fcab83780fe9b902d89d12d694b
yonggyo1125 <yonggyo00@kakao.com>
2025-02-11 15:30:26 +0900

    .

--- src/main/java/org/koreait/restaurant/controllers/NeighborSearch.java
+++ src/main/java/org/koreait/restaurant/controllers/NeighborSearch.java
@@ -3,7 +3,7 @@ package org.koreait.restaurant.controllers;
 import lombok.Data;
 
 @Data
-public class NeighborSearch {
+public class NeighborSearch extends RestaurantSearch {
     private double lat;
     private double lon;
     private int limit;
